e90625e0bbe5616cb73baf547e25f331e7998dc5
Isangeles <dev@isangeles.pl>
2020-04-16 23:06:47 +0200

    res: XML and JSON mappings for req structs

--- data/res/req.go
+++ data/res/req.go
@@ -23,40 +23,46 @@
 
 package res
 
+import (
+	"encoding/xml"
+)
+
 // Struct for reqs data.
 type ReqsData struct {
-	LevelReqs    []LevelReqData
-	GenderReqs   []GenderReqData
-	FlagReqs     []FlagReqData
-	ItemReqs     []ItemReqData
-	CurrencyReqs []CurrencyReqData
+	XMLName      xml.Name          `xml:"reqs" json:"-"`
+	LevelReqs    []LevelReqData    `xml:"level-req" json:"level-reqs"`
+	GenderReqs   []GenderReqData   `xml:"gender-req" json:"gender-reqs"`
+	FlagReqs     []FlagReqData     `xml:"flag-req" json:"flag-reqs"`
+	ItemReqs     []ItemReqData     `xml:"item-req" json:"item-reqs"`
+	CurrencyReqs []CurrencyReqData `xml:"currency-req" json:"currency-req"`
 }
 
 // Struct for level requirement data.
 type LevelReqData struct {
-	Min, Max int
+	Min int `xml:"min,attr" json:"min"`
+	Max int `xml:"max,attr" json:"max"`
 }
 
 // Struct for gender requirement data.
 type GenderReqData struct {
-	Gender string
+	Gender string `xml:"type,attr" json:"gender"`
 }
 
 // Struct for flag requirement data.
 type FlagReqData struct {
-	ID  string
-	Off bool
+	ID  string `xml:"id,attr" json:"id"`
+	Off bool   `xml:"off,attr" json:"off"`
 }
 
 // Struct for item requirement data.
 type ItemReqData struct {
-	ID     string
-	Amount int
-	Charge bool
+	ID     string `xml:"id,attr" json:"id"`
+	Amount int    `xml:"amount,attr" json:"amount"`
+	Charge bool   `xml:"charge,attr" json:"charge"`
 }
 
 // Struct for currency requirement data.
 type CurrencyReqData struct {
-	Amount int
-	Charge bool
+	Amount int  `xml:"amount,attr" json:"amount"`
+	Charge bool `xml:"charge,attr" json:"charge"`
 }
