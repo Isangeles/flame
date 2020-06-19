9d347994cf9a2c522f202e1a0b162ebb54686868
Isangeles <dev@isangeles.pl>
2020-06-19 20:29:26 +0200

    character: Use function

---
+++ module/character/use.go
@@ -0,0 +1,43 @@
+/*
+ * use.go
+ *
+ * Copyright 2020 Dariusz Sikora <dev@isangeles.pl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+package character
+
+import (
+	"github.com/isangeles/flame/module/effect"
+	"github.com/isangeles/flame/module/useaction"
+)
+
+// Use uses specified usable object.
+func (c *Character) Use(ob useaction.Usable) {
+	c.TakeModifiers(ob, ob.UseAction().UserMods()...)
+	for _, e := range ob.UseAction().UserEffects() {
+		c.TakeEffect(e)
+	}
+	if tar, ok := ob.(effect.Target); ok {
+		tar.TakeModifiers(ob, ob.UseAction().UserMods()...)
+		for _, e := range ob.UseAction().UserEffects() {
+			tar.TakeEffect(e)
+		}
+	}
+}
