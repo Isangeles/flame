8c0f65b86428235be73c17e0cf594b174bfdf987
Isangeles <dev@isangeles.pl>
2020-12-03 21:58:49 +0100

    res: fix for json mapping of restore flag in character data struct

--- data/res/character.go
+++ data/res/character.go
@@ -54,7 +54,7 @@ type CharacterData struct {
 	HP         int                   `xml:"hp,attr" json:"hp"`
 	Mana       int                   `xml:"mana,attr" json:"mana"`
 	Exp        int                   `xml:"exp,attr" json:"exp"`
-	Restore    bool                  `xml:"restore,attr" json:"resore"`
+	Restore    bool                  `xml:"restore,attr" json:"restore"`
 	Attributes AttributesData        `xml:"attributes" json:"attributes"`
 	Inventory  InventoryData         `xml:"inventory" json:"inventory"`
 	Equipment  EquipmentData         `xml:"equipment" json:"equipment"`
