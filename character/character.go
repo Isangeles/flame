32a9dae708dad4bb5b2c07c57ef295ea8cfbb16a
Isangeles <dev@isangeles.pl>
2021-11-07 17:53:26 +0100

    skill: applying passive effects on skill owner

--- character/character.go
+++ character/character.go
@@ -425,7 +425,7 @@ func (c *Character) Skills() (skills []*skill.Skill) {
 // AddSkill adds specified skill to characters
 // skills.
 func (c *Character) AddSkill(s *skill.Skill) {
-	s.UseAction().SetOwner(c)
+	s.SetOwner(c)
 	c.skills.Store(s.ID(), s)
 }
 
